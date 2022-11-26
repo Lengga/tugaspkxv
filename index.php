8b27ffa7f9cf6fdac4f1d40abcce227b1602ffa9
Lengga <112445638+Lengga@users.noreply.github.com>
2022-11-26 17:49:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lengga Priyani</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <header>
+        <input type="checkbox" name="" id="toggler">
+        <label for="toggler" class="fas fa-bars"></label>
+            <a href="#" class="logo" style="font-weight: bold">About Me<span>.</span></a>
+        <nav class="navbar">
+            <a href="#home" style="font-weight: bold">home</a>
+            <a href="#about" style="font-weight: bold">about</a>
+            <a href="#hobie" style="font-weight: bold">hobie</a>
+            <a href="#contact" style="font-weight: bold">contact</a>
+        </nav>
+        <div class="icons">
+            <a href="#" class="fas fa-heart"></a>
+            <a href="#" class="fas fa-user"></a>
+        </div>
+    </header>
+    <main>
+     <section class="home" id="home">
+        <div class="content">
+            <h3 style="font-weight: bold">About Me</h3>
+            <span style="font-weight: bold"> Lengga Priyani </span>
+            <p>Aku adalah seorang perempuan yang dilahirkan oleh kedua orangtua hebatku</p>
+            <a href="#" class="btn">Cari tau tentangku!</a>
+        </div>
+     </section>
+     <section class="about" id="about">
+        <div class="box-container">
+        <h1 class="heading" style="font-weight: bold"> <span> about </span> me </h1>
+        <div class="row">
+        <div class="video-container">
+            <img src="akulagi.jpg" width="800">
+        </div>
+        <div class="content">
+            <h3 style="font-weight: bold">My Biodata</h3>
+            <p>Hallo assalamualaikum semua, perkenalkan namaku Lengga Priyani. Teman-temanku biasa memanggilku "Lengga". Aku dilahirkan oleh orangtuaku di Kota Reyog yaitu Ponorogo pada tanggal 14 September 2001. Ayahku bernama Supriyadi, ayahku adalah seseorang yang dari dulu sampai sekarang masih menjadi motivasi dan penyemangat hidupku. Dan tentunya aku juga mempunyai ibu yang sangat hebat dalam segala hal, beliau bernama Supriyati. Ya, memang terbilang agak rancu gitu, karena nama ayah dan ibu ku hampir sama. Mungkin ini yang dinamakan berjodoh kali yaa. Aku merupakan anak kedua dari dua bersaudara. Aku mempunyai kakak perempuan yang bernama Galuh Priyani. Yaa betul, memang dalam keluargaku kami berempat hampir kembar kalau tentang nama, gatau juga sih apa motivasi kedua orangtua memberi nama kembar. Mungkin kalo manggil salah satu langsung menjawab semua gitu kali ya. Kakakku ini adalah orang yang sangat kalem dan mengalah kalo sama aku, aku sangat bersyukur dilahirkan untuk menjadi adik dari kakaku ini. Karena hampir semua hal yang mestinya dia ingin memiliki, namun jika aku menginginkan hal itu, maka kakakku akan mengalah dan memberikannya kepadaku. Hihi love banget sama my little familiy.</p>
+            <a href="#" class="btn">I love that</a>
+        </div>
+        </div>
+        </div>
+     </section>
+     <section class="icons-container">
+        <div class="icons">
+        <div class="info">
+            <h3 style="font-weight: bold">Mahasiswa Matematika</h3>
+            <span>2020</span>
+        </div>
+        </div>
+        <div class="icons">
+        <div class="info">
+            <h3 style="font-weight: bold">Anak Bungsu</h3>
+            <span>Dua Bersaudara</span>
+        </div>
+        </div>
+        <div class="icons">
+        <div class="info">
+            <h3 style="font-weight: bold">Keras Kepala</h3>
+            <span>bad attitude</span>
+        </div>
+        </div>
+        <div class="icons">
+        <div class="info">
+            <h3 style="font-weight: bold">Semamgat</h3>
+            <span>good attitude</span>
+        </div>
+        </div>
+     </section>
+     <section class="hobie" id="hobie">
+        <h1 class="heading" style="font-weight: bold"> My <span>Hobie</span> </h1>
+        <div class="box-container">
+        <div class="box">
+            <div class="image">
+                <img src="lari.png" alt="">
+            </div>
+            <div class="content">
+                <h3 style="font-weight: bold">Lari</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="beladiri.png" alt="">
+            </div>
+            <div class="content">
+                <h3 style="font-weight: bold">Beladiri</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="berenang.png" alt="">
+            </div>
+            <div class="content">
+                <h3 style="font-weight: bold">Berenang</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="healing.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3 style="font-weight: bold">Healing</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="nobar.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3 style="font-weight: bold">Nonton Film</h3>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="kuliner.png" alt="">
+            </div>
+            <div class="content">
+                <h3 style="font-weight: bold">Kulineran</h3>
+            </div>
+        </div>
+        </div>
+     </section>
+    </main>
+     <section class="contact" id="contact">
+        <h1 class="heading" style="font-weight: bold"> <span> contact </span> us </h1>
+        <div class="row">
+        <from action="welcome.php" method="post">
+            Name : <input type="text" placeholder="name" class="box">
+            Email : <input type="email" placeholder="email" class="box">
+            Number : <input type="number" placeholder="number" class="box">
+            Massage :<textarea name="" class="box" placeholder="massage" id="" cols="30" rows="10"></textarea>
+            <input type="submit" value="send message" class="btn">
+        </form>
+        <div class="image">
+            <img src="https://img.freepik.com/free-vector/illustration-female-character-wearing-hijab-working-office_10045-686.jpg?size=338&ext=jpg&ga=GA1.2.838532070.1666341093" >
+        </div>
+        </div>
+     </section>
+    <footer>
+     <section class="footer">
+        <div class="box-container">
+            <div class="box">
+            <h3 style="font-weight: bold">contact info</h3>
+            <a>0853 3036 9472</a>
+            <a>lenggapriyani63068@gmail.com</a>
+            <a>Ponorogo, Jawa Timur</a>
+            </div>
+         </div>
+         <div class="credit"> created by <span> Lengga Priyani</span> </div>
+     </section>
+    </footer>
+</body>
+</html>
